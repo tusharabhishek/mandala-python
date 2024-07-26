5734becde34e100bfbe4ff1eda31df57e97be43d
Tushar Abhishek <tusharabhishek8@gmail.com>
2024-07-26 22:21:16 +0530

    Implemented built-in atlas

--- src/mandala.py
+++ src/mandala.py
@@ -1,8 +1,13 @@
+import glob
+import os
 import tkinter
 from tkinter import ttk
 from typing import List
 
 from PIL import Image, ImageTk, ImageOps
+
+PRIMITIVE_PATH = '/home/tushar/work/mtp1/workspaces/mandala-python/src/assets/primitives'
+PRIMITIVE_BUTTON_SIZE = (36, 36)
 
 class Primitive:
   '''Data structure to represent a primitive in its most basic form.'''
@@ -13,6 +18,8 @@ class Primitive:
       # Copying image to a persistent variable because
       # access to the opened image file will be lost on exiting this block.
       self.image = image_file.copy()
+    
+    # Reference to image data (after processing)
     self.image = Primitive.process_image(self.image)
   
   def get_image(self):
@@ -30,7 +37,7 @@ class Primitive:
     '''
     # Mask the image with its inverted copy to add transparency
     # - whiter pixels are more transparent.
-    # Transparent pixels only show up on RGBA images.
+    # Transparent pixels appear on Tk buttons only when using RGBA images.
     result_image = Image.new('RGBA', image.size, (0, 0, 0, 0))
     mask_image = ImageOps.invert(image)
     result_image.paste(image, mask=mask_image)
@@ -38,15 +45,67 @@ class Primitive:
 
 
 class PrimitiveButton(ttk.Button):
-  def __init__(self, parent, primitive: Primitive, size: tuple[float, float], **keywords):
+  '''Button that adds a layer of its associated primitive to the canvas.'''
+  
+  def __init__(self, parent, *, primitive: Primitive, size: tuple[int, int], **keywords):
+    '''Create a button of given `size` (width x height) based on the `primitive` supplied.'''
     super().__init__(parent, **keywords)
 
     image = primitive.get_image()
     image.thumbnail(size)
+    # Reference to thumbnail shown on the button
     self.thumbnail = ImageTk.PhotoImage(image)
     
-    self['image'] = self.thumbnail
+    thumbnail_size = (self.thumbnail.width(), self.thumbnail.height())
+    # Padding needed to bring the button to size.
+    # Otherwise, the button will only be as large as the thumbnail.
+    padding = PrimitiveButton.get_padding(thumbnail_size, size)
+    
+    self.configure(image=self.thumbnail, padding=padding)
   
+  def get_padding(thumbnail_size: tuple[int, int], bounds_size: tuple[int, int]):
+    '''Return a tuple of padding needed to bring `thumbnail_size` to `bounds_size`.'''
+    x_diff, y_diff = bounds_size[0] - thumbnail_size[0], bounds_size[1] - thumbnail_size[1]
+    half_x, half_y = x_diff // 2, y_diff // 2
+
+    return (half_x, half_y, x_diff - half_x, y_diff - half_y)
+
+
+class AtlasFrame(ttk.LabelFrame):
+  def __init__(self, parent, *, title: str, columns: int, button_size: tuple[int, int], primitives: List[Primitive], **keywords):
+    super().__init__(parent, **keywords)
+
+    self.columns = columns
+    self.button_size = button_size
+
+    # Reference to primitive buttons added so far.
+    self.primitive_buttons: List[PrimitiveButton] = []
+
+    # Grid position to add the next PrimitiveButton at.
+    # A single value since the actual position
+    # will vary depending on the number of columns.
+    self.next_position: int = 0
+
+    for primitive in primitives:
+      self.add_primitive_button(primitive)
+    
+    self.configure(text=title)
+  
+  def add_primitive_button(self, primitive: Primitive):
+    row, column = self.next_position // self.columns, self.next_position % self.columns
+    
+    button = PrimitiveButton(self, primitive=primitive, size=self.button_size)
+    button.grid(row=row, column=column)
+    self.primitive_buttons.append(button)
+
+    self.next_position += 1
+
+
+def discover_primitives(dirpath):
+  primitive_paths = [f'{dirpath}/{path}' for path in glob.glob('*.png', root_dir=dirpath)]
+  primitive_paths.sort()
+  return primitive_paths
+
 
 def test():
   root = tkinter.Tk()
@@ -54,9 +113,10 @@ def test():
   content_frame = ttk.Frame(root)
   content_frame.grid()
   
-  p = Primitive('/home/tushar/work/mtp1/workspaces/mandala-python/src/assets/primitives/primitives-1-100dpi-002.png')
-  button = PrimitiveButton(content_frame, p, (50, 50))
-  button.grid()
+  primitives = [Primitive(path) for path in discover_primitives(PRIMITIVE_PATH)]
+
+  atlas_frame = AtlasFrame(content_frame, title='Built-in Primitives', columns=8, button_size=PRIMITIVE_BUTTON_SIZE, primitives=primitives)
+  atlas_frame.grid()
 
   root.mainloop()
 
